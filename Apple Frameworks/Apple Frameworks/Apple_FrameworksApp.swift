85159218da97cdc69f6d02034eb71440cc8ede26
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-13 13:39:50 +0200

    new project

---
+++ Apple Frameworks/Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Manyuchi, Carrington C on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
